97fdf43e87d364c587a9304630b4dfe2437b9587
Joana <joanajanefdez@gmail.com>
2022-08-09 16:11:02 +0200

    config inicial

---
+++ src/main/java/com/u23_SpringBasicMaven/U23SpringBasicMavenApplication.java
@@ -0,0 +1,13 @@
+package com.u23_SpringBasicMaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U23SpringBasicMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U23SpringBasicMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/u23_SpringBasicMaven/U23SpringBasicMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.u23_SpringBasicMaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U23SpringBasicMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
